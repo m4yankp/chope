2d13ffef38daf80925c083b7458582dbfe9d7ece
Mayank <m4yank@gmail.com>
2021-01-09 01:42:02 +0800

    User Registration and login

---
+++ public_html/app/Controllers/Users.php
@@ -0,0 +1,157 @@
+<?php namespace App\Controllers;
+
+use App\Models\UserModel;
+
+
+class Users extends BaseController
+{
+	private $cache;
+	function __construct()
+	{
+		$this->cache = \Config\Services::cache();
+	}
+	public function index()
+	{
+		$data = [];
+		helper(['form']);
+
+
+		if ($this->request->getMethod() == 'post') {
+			//let's do the validation here
+			$rules = [
+				'email' => 'required|min_length[6]|max_length[50]|valid_email',
+				'password' => 'required|min_length[8]|max_length[255]|validateUser[email,password]',
+			];
+
+			$errors = [
+				'password' => [
+					'validateUser' => 'Email or Password don\'t match'
+				]
+			];
+
+			if (! $this->validate($rules, $errors)) {
+				$data['validation'] = $this->validator;
+			}else{
+				$model = new UserModel();
+
+				$user = $model->where('email', $this->request->getVar('email'))
+											->first();
+
+				$this->setUserSession($user);
+				//$session->setFlashdata('success', 'Successful Registration');
+				return redirect()->to('./dashboard');
+
+			}
+		}
+
+		echo view('templates/header', $data);
+		echo view('login');
+		echo view('templates/footer');
+	}
+
+	private function setUserSession($user){
+		$data = [
+			'id' => $user['id'],
+			'firstname' => $user['firstname'],
+			'lastname' => $user['lastname'],
+			'email' => $user['email'],
+			'isLoggedIn' => true,
+		];
+
+		session()->set($data);
+		return true;
+	}
+
+	public function register(){
+		$data = [];
+		helper(['form']);
+
+		if ($this->request->getMethod() == 'post') {
+			//let's do the validation here
+			$rules = [
+				'firstname' => 'required|min_length[3]|max_length[20]',
+				'lastname' => 'required|min_length[3]|max_length[20]',
+				'email' => 'required|min_length[6]|max_length[50]|valid_email|is_unique[users.email]',
+				'password' => 'required|min_length[8]|max_length[255]',
+				'password_confirm' => 'matches[password]',
+			];
+			if (! $this->validate($rules)) {
+				$data['validation'] = $this->validator;
+			}else{
+				$model = new UserModel();
+
+				$newData = [
+					'firstname' => $this->request->getVar('firstname'),
+					'lastname' => $this->request->getVar('lastname'),
+					'email' => $this->request->getVar('email'),
+					'password' => $this->request->getVar('password'),
+				];
+				$model->save($newData);
+				$this->cache->save('registered','yes');
+				$session = session();
+				$session->setFlashdata('success', 'Successful Registration');
+				return redirect()->to('./');
+
+			}
+		}
+
+
+		echo view('templates/header', $data);
+		echo view('register');
+		echo view('templates/footer');
+	}
+
+	public function profile(){
+		
+		$data = [];
+		helper(['form']);
+		$model = new UserModel();
+
+		if ($this->request->getMethod() == 'post') {
+			//let's do the validation here
+			$rules = [
+				'firstname' => 'required|min_length[3]|max_length[20]',
+				'lastname' => 'required|min_length[3]|max_length[20]',
+				];
+
+			if($this->request->getPost('password') != ''){
+				$rules['password'] = 'required|min_length[8]|max_length[255]';
+				$rules['password_confirm'] = 'matches[password]';
+			}
+
+
+			if (! $this->validate($rules)) {
+				$data['validation'] = $this->validator;
+			}else{
+
+				$newData = [
+					'id' => session()->get('id'),
+					'firstname' => $this->request->getPost('firstname'),
+					'lastname' => $this->request->getPost('lastname'),
+					];
+					if($this->request->getPost('password') != ''){
+						$newData['password'] = $this->request->getPost('password');
+					}
+				$model->save($newData);
+
+				session()->setFlashdata('success', 'Successfuly Updated');
+				return redirect()->to('/profile');
+
+			}
+		}
+		var_dump($this->cache);
+		var_dump($this->cache->save('login','yes'));
+		$data['user'] = $model->where('id', session()->get('id'))->first();
+		echo view('templates/header', $data);
+		echo view('profile');
+		echo view('templates/footer');
+	}
+
+	public function logout(){
+		session()->destroy();
+		return redirect()->to('/');
+	}
+
+	//--------------------------------------------------------------------
+
+}
